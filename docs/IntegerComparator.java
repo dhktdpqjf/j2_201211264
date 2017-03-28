e2648b51a0524bfa6442d642706323463e6ddb03
ghyeon23 <jiih93@gmail.com>
2017-03-28 22:15:14 +0900

    Delete IntegerComparator.java

--- docs/IntegerComparator.java
+++
@@ -1,9 +0,0 @@
-package com.sd.sort.v1;
-import java.util.Comparator;
-
-public class IntegerComparator implements Comparator{  
-  public IntegerComparator(){}
-  public int compare(Object o1, Object o2){
-    return (Integer)o1-(Integer)o2;
-  }
-}
