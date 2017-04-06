6574a7b95b9f2751953859873afbfac27e248c2f
201211264 김지현 <jiih93@gmail.com>
2017-04-06 14:38:24 +0900

    Update BabyTurtle.java

--- src/com/sd/turtle/BabyTurtle.java
+++ src/com/sd/turtle/BabyTurtle.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 
 public class BabyTurtle extends Observable{
-  Turtle baby = new Turtle(); //BabyTurtle은 Turtle을 상속하는 것이 아니라 연관관계이기 때문에 Turtle이 가진 모든 속성과 함수를 물려받는 게 아니라 필요한 것만 가져다가 쓰면 됨.
+  Turtle baby = new Turtle(); 
   ArrayList<Observer> obs = new ArrayList<Observer>();
   Double myCurpos =  baby.getPos();
   
@@ -28,7 +28,6 @@ public class BabyTurtle extends Observable{
   
   protected void setChanged(){
     myCurpos = baby.getPos();
-    //notifyObservers();  //하고 여기서 불러도 되는데 main함수에서 부르기로!
   }
   
   public void move(int x, int y){
