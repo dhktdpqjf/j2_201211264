f00abb8d3aebe526dfe6a8a452cd194164bb446c
201211264 김지현 <jiih93@gmail.com>
2017-04-07 15:33:19 +0900

    Delete CalcAreaListener.java

--- src/src/com/sd/gui/CalcAreaListener.java
+++
@@ -1,9 +0,0 @@
-package com.sd.gui;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-public class CalcAreaListener implements ActionListener{
-  public void actionPerformed(ActionEvent e) {//ActionEvent가 뭔지 아직 모름.
-    System.out.println("hello");
-  }  
-}
