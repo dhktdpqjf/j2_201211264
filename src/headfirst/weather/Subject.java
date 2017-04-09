1e068e3dc89e3cff0352fc7a703489d162425186
201211264 김지현 <jiih93@gmail.com>
2017-04-09 22:48:36 +0900

    Delete Subject.java

--- src/headfirst/weather/Subject.java
+++
@@ -1,8 +0,0 @@
-package headfirst.observer.weather;
-
-public interface Subject{
-  public void registerObserver(Observer o);
-  public void removeObserver(Observer o);
-  public void notifyObserver();//observer였는데 s를 붙여서 다 알려주는 걸로
-  
-} 
