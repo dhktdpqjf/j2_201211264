d74881911a21a8e844cc9a2913c923b91acbb8bb
201211264 김지현 <jiih93@gmail.com>
2017-06-06 21:23:46 +0900

    Add files via upload

---
+++ src/com/sd/address/v1/Command.java
@@ -0,0 +1,5 @@
+package com.sd.dao.v2;
+
+public interface Command{
+  public void execute();
+}
---
+++ src/com/sd/address/v1/SimpleRemoteControl.java
@@ -0,0 +1,16 @@
+package com.sd.dao.v2;
+
+import java.util.*;
+
+public class SimpleRemoteControl{
+  Command slot;
+  
+  public SimpleRemoteControl(){}
+  
+  public void setCommand(Command command){
+    slot = command;
+  }
+  public void buttonWasPressed(){
+    slot.execute();
+  }
+}
